9014204091daca119e55caa2b9a53fe9db68f12a
coolhhc <coolhhc@gmail.com>
2013-08-16 16:07:45 +1000

    Initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedicineBox::Application.config.secret_key_base = '2dbebccc9ec68f92d65dc4b90f5de92fd1717c328a492aff8855cdbbbb3a4ac6a11edf258277511a5df3227bfc863385e8a7e9a30a5b5e07af17c5fa4487dcaa'
