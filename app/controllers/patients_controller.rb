6cee782f2779e14b731561074b979f7197dde963
coolhhc <coolhhc@gmail.com>
2013-08-24 15:09:26 +1000

    First Production Version

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,5 +4,13 @@ class PatientsController < ApplicationController
     end
 
     def new
+        @patient = Patient.new
+    end
+
+    def create
+        @patient = Patient.new(params[:patient])
+        #if @patient = Patient.create(params[:patient])
+        #    redirect_to new_patient_path
+        #end
     end
 end
