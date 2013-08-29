b5fe22dfda6a1af14b978a2a69511d3856da9b69
coolhhc <coolhhc@gmail.com>
2013-08-29 16:50:13 +1000

    Hack form patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -12,16 +12,31 @@ class PatientsController < ApplicationController
 
     def new
         @patient = Patient.new
+    end
+
+    def create
+        #@patient = Patient.new(params[:patient])
+        puts "name: " + params[:patient][:name] + "------------------------------------------------------"
+
+        #hack
+        if @patient = Patient.create(name: params[:patient][:name])
+            redirect_to root_path
+        end
+        
+        #if @patient = Patient.create(params[patient_params])
+        #    redirect_to new_patient_path
+        #end
+    end
+
+    def json_stuff # Need to add routing
         #json_object = JSON.parse(open("http://localhost:3000/patients/224.json").read)
         #puts json_object
         #recent_posts = HTTParty.get("http://localhost:3000/patients/224.json")
         #puts recent_posts
     end
 
-    def create
-        @patient = Patient.new(params[:patient])
-        #if @patient = Patient.create(params[:patient])
-        #    redirect_to new_patient_path
-        #end
+    private
+    def patient_params
+        params.require(:patient).permit(:name)
     end
 end
