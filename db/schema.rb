0c1251fafe542f13c5613b8db348fc269e44911b
coolhhc <coolhhc@gmail.com>
2013-09-10 00:00:48 +1000

    Setup Survey rule form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822151123) do
+ActiveRecord::Schema.define(version: 20130909113709) do
 
   create_table "charts", force: true do |t|
     t.integer  "patient_id"
@@ -56,10 +56,25 @@ ActiveRecord::Schema.define(version: 20130822151123) do
     t.datetime "updated_at"
   end
 
+  create_table "rules", force: true do |t|
+    t.integer  "survey_id"
+    t.text     "condition"
+    t.text     "true"
+    t.text     "false"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "schedules", force: true do |t|
     t.integer  "patient_id"
     t.text     "description"
     t.datetime "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "surveys", force: true do |t|
+    t.text     "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
