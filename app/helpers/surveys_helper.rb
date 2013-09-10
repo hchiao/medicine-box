cdd8acf4667b4f481a88678bef32bf09b8029f74
coolhhc <coolhhc@gmail.com>
2013-09-11 00:27:37 +1000

    Follow the "complex-form" project that I cloned to fix the dynamic form

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,2 +1,9 @@
 module SurveysHelper
+    def link_to_add_fields(name, f, association)
+        new_object = f.object.class.reflect_on_association(association).klass.new
+        fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |form_rule|
+            render(association.to_s.singularize, :f => form_rule)
+        end
+        link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+    end
 end
