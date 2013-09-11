be150ccd7f3895a8a8bf074d1380f324ec6d3139
coolhhc <coolhhc@gmail.com>
2013-09-11 14:50:49 +1000

    Before starting a new client_survey branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 MedicineBox::Application.routes.draw do
-  resources :surveys
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,6 +7,7 @@ MedicineBox::Application.routes.draw do
   # root 'welcome#index'
   root 'patients#show'
   resources :patients
+  resources :surveys
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
