6a923b1e2ee672a2b35162ec1598dd51c700f3a5
coolhhc <coolhhc@gmail.com>
2013-09-11 11:35:08 +1000

    Working form with dynamic fields, now work on processing the fields.

--- vendor/assets/javascripts/defaults.js
+++ vendor/assets/javascripts/defaults.js
@@ -1,3 +1,9 @@
+function remove_fields(link) {
+  alert($(link).up(".fields"));
+  $(link).previous("input[type=hidden]").value = "1";
+  $(link).up(".fields").hide();
+}
+
 function add_fields(link, association, content) {
     var new_id = new Date().getTime();
     var regexp = new RegExp("new_" + association, "g");
