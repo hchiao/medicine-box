0370146393d194c4bc5b27df30ce6de539434f03
coolhhc <coolhhc@gmail.com>
2013-09-15 17:16:11 +1000

    Setting root

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -15,7 +15,7 @@ class SurveysController < ApplicationController
   # GET /surveys/new
   def new
     @survey = Survey.new
-    @survey.rules.build
+    3.times{@survey.rules.build}
   end
 
   # GET /surveys/1/edit
